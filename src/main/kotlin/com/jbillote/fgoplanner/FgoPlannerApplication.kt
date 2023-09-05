04333d1f357e71ab3bc73d17befe3dfd6b28fc00
jbillote <jbillote@users.noreply.github.com>
2023-09-05 01:29:31 -0700

    Initial Commit

---
+++ src/main/kotlin/com/jbillote/fgoplanner/FgoPlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.jbillote.fgoplanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FgoPlannerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FgoPlannerApplication>(*args)
+}
